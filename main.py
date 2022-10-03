fa7c6339d07a18b79d685a0f6d5c830e489cd010
Nikolay <nd.denisoff@gmail.com>
2022-10-03 16:29:44 +0300

    PIL 2.0

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 
+from PIL.Image import Transpose
+from PyQt5 import uic
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel, QCheckBox, QLCDNumber, QMainWindow
-from ui.anti import Ui_MainWindow
+from ui.interface import Ui_MainWindow
+from PIL import Image
 
 
 class Example(QMainWindow, Ui_MainWindow):
@@ -10,23 +14,57 @@ class Example(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.initUI()
 
-    def initUI(self):
-        self.pushButton.clicked.connect(self.compare)
+    def set_image(self, name):
+        self.pixmap = QPixmap(name).scaled(600, 500)
+        self.label = QLabel('')
+        self.label.setPixmap(self.pixmap)
 
-    def compare(self):
-        cou = 0
-        ln1 = self.text1.toPlainText().replace(' ', '').replace('\n', '').replace('\t', '')
-        ln2 = self.text2.toPlainText().replace(' ', '').replace('\n', '').replace('\t', '')
-        for i in range(min(len(ln1), len(ln2))):
-            if ln1[i] == ln2[i]:
-                cou += 1
-        mx = max(len(ln1), len(ln2))
-        if mx:
-            self.statusbar.showMessage(f'Код похож на {round(cou * 100 / mx, 2)}%')
-            if cou * 100 / mx > float(self.doubleSpinBox.text().replace(',', '.')):
-                self.statusbar.setStyleSheet('background-color : red')
-            else:
-                self.statusbar.setStyleSheet('background-color : green')
+    def initUI(self):
+        uic.loadUi('ui/interface.ui', self)
+        self.setWindowTitle('PIL 2.0')
+        self.rotat = 0
+        Image.open('based.png').save('changed.png')
+        self.set_image('changed.png')
+        self.verticalLayout_3.addWidget(self.label)
+        self.color.buttonClicked.connect(self.change_color)
+        self.rotate.buttonClicked.connect(self.rotates)
+
+    def rotates(self, button: QPushButton):
+        if button.objectName() == 'vs':
+            Image.open('changed.png').transpose(Transpose.ROTATE_90).save('changed.png')
+            self.rotat += 1
+        else:
+            Image.open('changed.png').transpose(Transpose.ROTATE_270).save('changed.png')
+            self.rotat += 3
+
+        label = self.label
+        self.set_image("changed.png")
+        self.verticalLayout_3.replaceWidget(label, self.label)
+
+    def change_color(self, button: QPushButton):
+        if button.text() == 'R':
+            colors = [1, 0, 0]
+        elif button.text() == 'G':
+            colors = [0, 1, 0]
+        elif button.text() == 'B':
+            colors = [0, 0, 1]
+        else:
+            colors = [1, 1, 1]
+
+        im = Image.open("based.png")
+        for i in range(self.rotat):
+            im = im.transpose(Transpose.ROTATE_90)
+        pixels = im.load()
+        x, y = im.size
+
+        for i in range(x):
+            for j in range(y):
+                r, g, b = pixels[i, j]
+                pixels[i, j] = r * colors[0], g * colors[1], b * colors[2]
+        im.save('changed.png')
+        label = self.label
+        self.set_image("changed.png")
+        self.verticalLayout_3.replaceWidget(label, self.label)
 
 
 if __name__ == '__main__':
